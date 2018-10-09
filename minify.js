3f3881755bf0236071719904fe25dd385e4143cc
Telmo Barros <telmo.20@gmail.com>
2018-10-09 05:03:22 +0200

    dist

--- minify.js
+++ minify.js
@@ -26,3 +26,4 @@ fs.createReadStream('./partials/index.htm').pipe(fs.createWriteStream('./dist/pa
 fs.createReadStream('./partials/resultado.htm').pipe(fs.createWriteStream('./dist/partials/resultado.htm'));
 fs.createReadStream('./index.html').pipe(fs.createWriteStream('./dist/index.html'));
 fs.createReadStream('./index.html').pipe(fs.createWriteStream('./dist/404.html'));
+fs.createReadStream('./index.html').pipe(fs.createWriteStream('./404.html'));
